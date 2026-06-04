6699eeabf1d47a338e5ed5d202d980b011d5d653
Ayush singh <ayush@bigcity.in>
2026-06-04 16:50:50 +0530

    commit: Started select box

---
+++ src/components/molecules/selectBox/index.tsx
@@ -0,0 +1,30 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export const SelectboxComponent = (data,
+  labelKey,
+
+  selected,
+  onChange,
+
+  multiSelect = false,
+
+  placeHolder,
+  searchBar = false,
+  searchPlaceholder = 'Search....',
+  noResultsText,
+  showChips = false,
+
+  label
+  disabled = false,
+  error,
+  required = false,
+
+  style,
+  testId,) => {
+  return (
+    <View>
+      
+    </View>
+  );
+};
---
+++ src/components/molecules/selectBox/types.d.ts
@@ -0,0 +1,25 @@
+import { ViewStyle } from 'react-native';
+
+export interface SelectBoxProps<T = Record<string, any>> {
+  data: T[];
+  labelKey: keyof T;
+
+  selected: T[] | null;
+  onChange: (selected: T[]) => void;
+
+  multiSelect?: boolean;
+
+  placeHolder: string;
+  searchBar?: boolean;
+  searchPlaceholder?: string;
+  noResultsText?: string;
+  showChips?: boolean;
+
+  label: string;
+  disabled?: boolean;
+  error?: string;
+  required?: boolean;
+
+  style?: ViewStyle;
+  testId?: string;
+}
